8bd28aa14ebc39c350c042007020a1bcd04c3387
juniway <juniway@sina.com>
2016-09-18 20:29:39 +0800

    updates

--- Sort/merge_sort.c
+++ Sort/merge_sort.c
@@ -1,27 +1,25 @@
 Merge Sort
 
 /*
- The merge sort splits the list to be sorted into two equal halves, and places them in 
- separate arrays. Each array is recursively sorted, and then merged back together to 
- form the final sorted list. Like most recursive sorts, the merge sort has an algorithmic 
- has an complexity of O(n log n).
- Elementary implementations of the merge sort make use of three arrays - one for each half 
- of the data set and one to store the sorted list in. The below algorithm merges the arrays
- in-place, so only two arrays are required. There are non-recursive versions of the merge sort, 
- but they don't yield any significant performance enhancement over the recursive algorithm on 
- most machines.
+The merge sort splits the list to be sorted into two equal halves, and places them in
+separate arrays. Each array is recursively sorted, and then merged back together to
+form the final sorted list. Like most recursive sorts, the merge sort has an algorithmic
+has an complexity of O(n log n).
+
+Elementary implementations of the merge sort make use of three arrays - one for each half
+of the data set and one to store the merged sorted list in. The below algorithm merges the arrays
+in-place, so only two arrays are required. There are non-recursive versions of the merge sort,
+but they don't yield any significant performance enhancement over the recursive algorithm on
+most machines.
 
 
 Pros: Marginally faster than the heap sort for larger sets.
 
 Cons: At least twice the memory requirements of the other sorts; recursive.
- */
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
-
-
-
 
 void mergeSort(int numbers[], int temp[], int array_size);
 void m_sort(int numbers[], int temp[], int left, int right);
@@ -31,87 +29,163 @@ int numbers[10];
 int temp[10];
 
 
+int main(){
+    int i;
+    printf("Enter the numbers you want to sort");
+    for(i = 0; i < 10; i++)
+        scanf("%d", &numbers[i]);
+
+    //perform merge sort on array
+    mergeSort(numbers, temp, NUM_ITEMS);
+    printf("Done with sort.\n");
+    for (i = 0; i < NUM_ITEMS; i++)
+        printf("%i\n", numbers[i]);
+}
+
+void mergeSort(int numbers[], int temp[], int array_size){
+    m_sort(numbers, temp, 0, array_size - 1);
+}
+
+void m_sort(int numbers[], int temp[], int left, int right){
+    int mid;
+
+    if (right > left){
+        mid = (right + left) / 2;
+        m_sort(numbers, temp, left, mid);
+        m_sort(numbers, temp, mid+1, right);
+        merge(numbers, temp, left, mid+1, right);
+    }
+}
+
+void merge(int numbers[], int temp[], int left, int mid, int right){
+    int i, left_end, num_elements, tmp_pos;
+
+    left_end = mid - 1;
+    tmp_pos = left;
+    num_elements = right - left + 1;
+
+    while ((left <= left_end) && (mid <= right)){
+        if (numbers[left] <= numbers[mid]){
+            temp[tmp_pos] = numbers[left];
+            tmp_pos = tmp_pos + 1;
+            left = left +1;
+        }
+        else{
+            temp[tmp_pos] = numbers[mid];
+            tmp_pos = tmp_pos + 1;
+            mid = mid + 1;
+        }
+    }
+
+    while (left <= left_end){
+        temp[tmp_pos] = numbers[left];
+        left = left + 1;
+        tmp_pos = tmp_pos + 1;
+    }
+    while (mid <= right){
+        temp[tmp_pos] = numbers[mid];
+        mid = mid + 1;
+        tmp_pos = tmp_pos + 1;
+    }
+
+    for (i=0; i <= num_elements; i++){
+        numbers[right] = temp[right];
+        right = right - 1;
+    }
+}
+
+
+
+/* C program for Merge Sort */
+#include<stdlib.h>
+#include<stdio.h>
+
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r){
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+
+    // create temp arrays
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+
+    // Merge the temp arrays back into arr[l..r]
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2){
+        if (L[i] <= R[j]){
+            arr[k] = L[i];
+            i++;
+        }
+        else{
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if there are any
+    while (i < n1){
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if there are any
+    while (j < n2){
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+/* l is for left index and r is right index of the
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r){
+    if (l < r){
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r-l) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+/* UTILITY FUNCTIONS */
+/* Function to print an array */
+void printArray(int A[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+/* Driver program to test above functions */
 int main()
 {
-  int i;
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
 
-  
-  printf("Enter the numbers you want to sort");
-  for(i=0;i<10;i++)
-   scanf("%d",&numbers[i]);
-  
-  //perform merge sort on array
-  mergeSort(numbers, temp, NUM_ITEMS);
+    printf("Given array is \n");
+    printArray(arr, arr_size);
 
-  printf("Done with sort.\n");
+    mergeSort(arr, 0, arr_size - 1);
 
-  for (i = 0; i < NUM_ITEMS; i++)
-    printf("%i\n", numbers[i]);
-}
-
-
-void mergeSort(int numbers[], int temp[], int array_size)
-{
-  m_sort(numbers, temp, 0, array_size - 1);
-}
-
-
-
-void m_sort(int numbers[], int temp[], int left, int right)
-{
-  int mid;
-
-  if (right > left)
-  {
-    mid = (right + left) / 2;
-    m_sort(numbers, temp, left, mid);
-    m_sort(numbers, temp, mid+1, right);
-
-    merge(numbers, temp, left, mid+1, right);
-  }
-}
-
-
-void merge(int numbers[], int temp[], int left, int mid, int right)
-{
-  int i, left_end, num_elements, tmp_pos;
-
-  left_end = mid - 1;
-  tmp_pos = left;
-  num_elements = right - left + 1;
-
-  while ((left <= left_end) && (mid <= right))
-  {
-    if (numbers[left] <= numbers[mid])
-    {
-      temp[tmp_pos] = numbers[left];
-      tmp_pos = tmp_pos + 1;
-      left = left +1;
-    }
-    else
-    {
-      temp[tmp_pos] = numbers[mid];
-      tmp_pos = tmp_pos + 1;
-      mid = mid + 1;
-    }
-  }
-
-  while (left <= left_end)
-  {
-    temp[tmp_pos] = numbers[left];
-    left = left + 1;
-    tmp_pos = tmp_pos + 1;
-  }
-  while (mid <= right)
-  {
-    temp[tmp_pos] = numbers[mid];
-    mid = mid + 1;
-    tmp_pos = tmp_pos + 1;
-  }
-
-  for (i=0; i <= num_elements; i++)
-  {
-    numbers[right] = temp[right];
-    right = right - 1;
-  }
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
+    return 0;
 }
